74d25135764b516a7ebd84afb43f5353f2207661
youngsik823 <youngsik823@naver.com>
2024-06-03 23:43:05 +0900

    initial commit

---
+++ src/main/java/com/csrp/csrp/CsrpApplication.java
@@ -0,0 +1,13 @@
+package com.csrp.csrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsrpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csrp/csrp/CsrpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csrp.csrp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
