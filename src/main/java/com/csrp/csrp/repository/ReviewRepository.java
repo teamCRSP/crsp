698b12a69205ac4ea2b2c44dd6b4210fa5297219
youngsik823 <youngsik823@naver.com>
2024-06-06 04:56:41 +0900

    feat: ReviewRepository 추가

---
+++ src/main/java/com/csrp/csrp/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.csrp.csrp.repository;
+
+import com.csrp.csrp.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
