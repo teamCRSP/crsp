a188a2dd9ddef12a74faa1e008040f3775938cdf
youngsik823 <youngsik823@naver.com>
2024-06-15 20:32:50 +0900

    fix: List<Ticket> findByEndDateBefore(LocalDateTime now) 추가

--- src/main/java/com/csrp/csrp/repository/TicketRepository.java
+++ src/main/java/com/csrp/csrp/repository/TicketRepository.java
@@ -5,6 +5,7 @@ import com.csrp.csrp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +13,6 @@ import java.util.Optional;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
   List<Ticket> findByUser(User user);
+
+  List<Ticket> findByEndDateBefore(LocalDateTime now);  // 지금 시간이 endDate 와 같은 ticket 찾기
 }
