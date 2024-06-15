9e148e8e79cd4f969c48cd127a9253ab05914556
youngsik823 <youngsik823@naver.com>
2024-06-15 18:27:21 +0900

    fix: amount 제거 concertLocInfoId, concertLocId, concertSeat, concertSeatPrice, ticketCount, startDate, endDate 추가

--- src/main/java/com/csrp/csrp/dto/request/PaymentRequestDTO.java
+++ src/main/java/com/csrp/csrp/dto/request/PaymentRequestDTO.java
@@ -4,6 +4,7 @@ import com.csrp.csrp.entity.PaymentHistory;
 import com.csrp.csrp.entity.ReservationHistory;
 import com.csrp.csrp.entity.User;
 import com.csrp.csrp.type.PaymentStatus;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,19 +20,29 @@ import java.time.LocalDateTime;
 public class PaymentRequestDTO {
 
   @NotNull
-  private int amount;
+  private Long concertLocInfoId;
 
   @NotNull
-  private Long ReservationHistoryId;
+  private Long concertLocId;
 
-  public PaymentHistory toEntity(User user, ReservationHistory reservationHistory) {
-    return PaymentHistory.builder()
-        .amount(this.amount)
-        .paymentDate(LocalDateTime.now())
-        .paymentStatus(PaymentStatus.COMPLETED)
-        .user(user)
-        .reservationHistory(reservationHistory)
-        .build();
-  }
+  @NotNull
+  private String concertSeat;
+
+  @NotNull
+  private int concertSeatPrice;
+
+  @NotNull
+  @Min(1)
+  private int ticketCount;  // 개수
+
+  @NotNull
+  private LocalDateTime startDate;
+
+  @NotNull
+  private LocalDateTime endDate;
+
+  @NotNull
+  private Long reservationHistoryId;
+
 
 }
