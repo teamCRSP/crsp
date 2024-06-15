a7e43b12693f23deae6dea77d8f7f45a000f94c5
youngsik823 <youngsik823@naver.com>
2024-06-16 02:25:18 +0900

    feat: 테스트용 컨트롤러

--- src/main/java/com/csrp/csrp/controller/UserController.java
+++ src/main/java/com/csrp/csrp/controller/UserController.java
@@ -24,11 +24,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class UserController {
   private final UserService userService;
 
-  @GetMapping("/")
-  public ResponseEntity<String> test() {
-    return ResponseEntity.ok("Hello from Spring Boot Backend!");
-  }
-
 // 회원가입
   @PostMapping("/signUp")
   public ResponseEntity<?>signUp(
