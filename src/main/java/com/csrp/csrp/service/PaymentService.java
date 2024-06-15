bb19bf80eace87929b8911f1222a7af936fcf162
youngsik823 <youngsik823@naver.com>
2024-06-15 18:25:34 +0900

    fix: 결제 등록 기능 수정  결제 성공 -> 예매 내역 등록 -> 예매 내역 상세 등록 -> 티켓 발부 -> 결제 내역 등록

--- src/main/java/com/csrp/csrp/service/PaymentService.java
+++ src/main/java/com/csrp/csrp/service/PaymentService.java
@@ -2,10 +2,7 @@ package com.csrp.csrp.service;
 
 import com.csrp.csrp.dto.request.PaymentRequestDTO;
 import com.csrp.csrp.dto.response.PaymentHistoryResponseDTO;
-import com.csrp.csrp.entity.ConcertInfo;
-import com.csrp.csrp.entity.PaymentHistory;
-import com.csrp.csrp.entity.ReservationHistory;
-import com.csrp.csrp.entity.User;
+import com.csrp.csrp.entity.*;
 import com.csrp.csrp.exception.CustomException;
 import com.csrp.csrp.repository.*;
 import com.csrp.csrp.token.TokenUserInfo;
@@ -26,16 +23,66 @@ public class PaymentService {
   private final PaymentRepository paymentRepository;
   private final UserRepository userRepository;
   private final ReservationHistoryRepository reservationHistoryRepository;
+  private final ReservationDetailRepository reservationDetailRepository;
   private final ConcertInfoRepository concertInfoRepository;
+  private final ConcertLocInfoRepository concertLocInfoRepository;
+  private final TicketRepository ticketRepository;
+  private final ReservationHistoryService reservationHistoryService;
+  private final TicketService ticketService;
 
   // 결제 등록
-  public void paymentDone(PaymentRequestDTO request, TokenUserInfo tokenUserInfo) {
-    User user = userRepository.findById(tokenUserInfo.getId())
-        .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXISTS_USER));
-    ReservationHistory reservationHistory = reservationHistoryRepository.findById(request.getReservationHistoryId())
-        .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXISTS_RESERVATION_HISTORY));
-    PaymentHistory entity = request.toEntity(user, reservationHistory);
-    paymentRepository.save(entity);
+  // 결제 성공 -> 예매 내역 등록 -> 예매 내역 상세 등록 -> 티켓 발부 -> 결제 내역 등록
+  public void paymentDone(List<PaymentRequestDTO> request, TokenUserInfo tokenUserInfo) {
+    ReservationHistory reservationHistory = null;
+    ReservationDetail reservationDetail = null;
+    List<Ticket> tickets = null;
+    try {
+      int amount = 0;
+      for (PaymentRequestDTO paymentRequestDTO : request) {
+        amount += (paymentRequestDTO.getTicketCount() * paymentRequestDTO.getConcertSeatPrice());
+      }
+      ConcertInfo concertInfo = concertInfoRepository.findById(request.get(0).getConcertLocId())
+          .orElseThrow(() -> new CustomException(ErrorCode.CONCERT_NOT_FOUND));
+      User user = userRepository.findById(tokenUserInfo.getId())
+          .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXISTS_USER));
+      // 예매 내역 등록
+      reservationHistory = ReservationHistory.builder()
+          .amount(amount)
+          .user(user)
+          .concertInfo(concertInfo)
+          .build();
+      ReservationHistory save = reservationHistoryRepository.save(reservationHistory);
+      // 예매 내역 상세 등록
+      for (PaymentRequestDTO paymentRequestDTO : request) {
+        reservationDetail = ReservationDetail.builder()
+            .endDate(paymentRequestDTO.getEndDate())
+            .startDate(paymentRequestDTO.getStartDate())
+            .concertSeat(paymentRequestDTO.getConcertSeat())
+            .concertSeatPrice(paymentRequestDTO.getConcertSeatPrice())
+            .ticketCount(paymentRequestDTO.getTicketCount())
+            .concertLocInfo(concertLocInfoRepository.findById(paymentRequestDTO.getConcertLocId())
+                .orElseThrow(() -> new CustomException(ErrorCode.CONCERT_LOCATION_NOT_FOUND)))
+            .reservationHistory(reservationHistoryRepository.findById(paymentRequestDTO.getReservationHistoryId())
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXISTS_RESERVATION_HISTORY)))
+            .build();
+        reservationDetailRepository.save(reservationDetail);
+        // 티켓 발부
+        tickets = ticketService.getTicket(paymentRequestDTO, tokenUserInfo);
+      }
+
+
+
+      // 결제 내역 등록
+      paymentRepository.save(PaymentHistory.toEntity(user, reservationHistory));
+    } catch (Exception e) { // 결제 실패 시 예매 내역 및 티켓 삭제
+      if (reservationHistory != null) {
+        reservationHistoryRepository.delete(reservationHistory);
+      }
+      if (tickets != null) {
+          ticketRepository.deleteAll(tickets);
+      }
+      throw new RuntimeException(e);
+    }
 
   }
 
@@ -51,7 +98,8 @@ public class PaymentService {
       ConcertInfo concertInfo = concertInfoRepository.findById(reservationHistory.getConcertInfo().getId())
           .orElseThrow(() -> new CustomException(ErrorCode.CONCERT_NOT_FOUND));
       PaymentHistoryResponseDTO paymentHistoryResponseDTO = new PaymentHistoryResponseDTO(concertInfo, paymentHistory);
-    toDto.add(paymentHistoryResponseDTO);
+
+      toDto.add(paymentHistoryResponseDTO);
     }
     return toDto;
   }
