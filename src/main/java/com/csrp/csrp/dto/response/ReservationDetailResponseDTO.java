3b8427b2159bf103b93a3c20c184184b278cf3dc
youngsik823 <youngsik823@naver.com>
2024-06-15 18:23:20 +0900

    fix: 콘서트 시작 및 종료 추가

--- src/main/java/com/csrp/csrp/dto/response/ReservationDetailResponseDTO.java
+++ src/main/java/com/csrp/csrp/dto/response/ReservationDetailResponseDTO.java
@@ -26,7 +26,9 @@ public class ReservationDetailResponseDTO {
 
   private int ticketCount;
 
-  private LocalDateTime concertDate;
+  private LocalDateTime startDate;
+
+  private LocalDateTime endDate;
 
   private String concertLocation;
 
@@ -40,7 +42,8 @@ public class ReservationDetailResponseDTO {
     this.concertType = concertInfo.getConcertType();
     this.seatInfo = reservationDetail.getConcertSeat();
     this.ticketCount = reservationDetail.getTicketCount();
-    this.concertDate = reservationDetail.getConcertDate();
+    this.startDate = reservationDetail.getStartDate();
+    this.endDate = reservationDetail.getEndDate();
     this.concertSeatPrice = reservationDetail.getConcertSeatPrice();
     this.concertLocation = reservationDetail.getConcertLocInfo().getLocation().get(0);  /////////////////////////// 고치기
     this.concertInfoId = concertInfo.getId();
